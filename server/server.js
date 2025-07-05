d674c9871b79504edf70f3b94f4212d2a0f89376
Vansh Ruhela <167776670+ThunderBolt4931@users.noreply.github.com>
2025-07-05 14:00:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1012,6 +1012,7 @@ app.get('/auth/google', (req, res) => {
             'https://www.googleapis.com/auth/gmail.send',
             'https://www.googleapis.com/auth/gmail.labels',
             'https://www.googleapis.com/auth/gmail.modify',
+            'https://www.googleapis.com/auth/gmail.compose',
             'https://www.googleapis.com/auth/calendar.events',
             'https://www.googleapis.com/auth/calendar.readonly',
             'https://www.googleapis.com/auth/documents',
